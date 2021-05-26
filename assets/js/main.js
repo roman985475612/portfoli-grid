2ef1998bed06fe7d025ebdbd49416c91775a1be6
Roman <roman985475612@gmail.com>
2021-05-26 16:11:43 +0300

    Added Client side validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,28 +3,54 @@ window.addEventListener('DOMContentLoaded', () => {
     const tabList = document.querySelectorAll('.port-item')
     const tabSection = document.querySelectorAll('.tab-section');
 
-    for (let i of tabList) {
-        i.addEventListener('click', showTab)
-    }
+    tabList.forEach(i => i.addEventListener('click', showTab))
 
-    function showTab(e) {
+    function showTab() {
         hideSections()
 
         this.classList.add('active')
 
-        curSection = document.getElementById(this.dataset.section)
-        curSection.classList.add('active')
+        document.getElementById(this.dataset.section)
+                .classList
+                .add('active')
     }
 
     function hideSections() {
-        for (let s of tabSection) {
-            s.classList.remove('active')
+        tabList.forEach(t => t.classList.remove('active'))
+        tabSection.forEach(s => s.classList.remove('active'))
+    }
+
+    const contactForm = document.getElementById('contact-form')
+    const fields = document.querySelectorAll('.form-control')
+
+    fields.forEach(f => f.addEventListener('change', validateField))
+
+    function validateField() {
+        if (this.value === '') {
+            if (this.classList.contains('is-valid')) {
+                this.classList.remove('is-valid')
+            }
+            this.classList.add('is-invalid')
+        } else {
+            if (this.classList.contains('is-invalid')) {
+                this.classList.remove('is-invalid')
+            }
+            this.classList.add('is-valid')
         }
 
-        for (let t of tabList) {
-            t.classList.remove('active')
-        }
     }
+
+    contactForm.addEventListener('submit', e => {
+        e.preventDefault()
+
+        fields.forEach(f => {
+            if (f.value === '') {
+                f.classList.add('is-invalid')
+            } else {
+                f.classList.add('is-valid')
+            }
+        })
+    })
 
     baguetteBox.run('.gallery')
 })
